5e42c1766c21311601bf78dc4413a47e8956c96e
Vít Dobrovský <vit.dobrovsky.023@pslib.cz>
2024-12-11 13:14:47 +0100

    class model

---
+++ generator.py
@@ -0,0 +1,168 @@
+from fastapi import UploadFile, File, HTTPException
+from PIL import Image
+import zipfile
+import io
+import os
+
+class Image_generator:
+    def __init__(self, transparent: bool, responsive: bool, max_width: int, generate_HTML: bool = True, generate_CSS: bool = True):
+        
+        # sizes
+        self.sizes = {
+        "small": 420,
+        "medium": 960,
+        "extra_large": 1920
+        }
+        self.max_width = max_width
+        self.minimmum_step_size = 300
+        self.responsive = responsive
+
+        # format converting
+        self.transparent = transparent
+        self.available_formats = self.get_available_formats(transparent)
+
+        # generate hml 
+        self.html_sources = self.initialize_HTML_sources(self.available_formats)
+        self.names = []
+        self.allow_HTML_generation = generate_HTML
+
+        # generate css
+        self.allow_CSS_generation = generate_CSS
+        
+    def get_available_formats(self, transparent: bool) -> list:
+        """
+        Get the list of available image formats based on the transparency flag
+        """
+        return ["png" if transparent else "jpeg", "webp"]
+
+    def initialize_HTML_sources(self, formats: list):
+        """
+        Initialize the HTML sources dictionary with empty lists for each format
+        """
+        sources = {}
+
+        for format in formats:
+            sources[format] = []
+        
+        return sources
+
+    def generate_HTML(self, available_formats: list, html_sources: dict) -> str:
+        """
+        Generate a responsive HTML picture element with source elements for each format and zize in an img tag
+        """
+        html = "<picture>"
+
+        # Generate srcset attributes for each format in reverse order (to prioritize the last format)
+        for format, sources in reversed(html_sources.items()):
+            srcset = ", ".join(sources)
+            html += f'<source srcset="{srcset}" type="image/{format}">'
+
+        # Find the largest image across all formats for the img src attribute
+        largest_image = max(
+            html_sources[available_formats[0]],
+            key=lambda x: int(x.split('-')[1].split('.')[0])
+        ).split(' ')[0]
+
+        # Generate sizes attribute based on the available sizes in html_sources
+        self.sizes = ", ".join(
+            f"(max-width: {int(img.split('-')[1].split('.')[0])}px) {int(img.split('-')[1].split('.')[0])}px"
+            for img in sorted(
+                {img for imgs in html_sources.values() for img in imgs},
+                key=lambda x: int(x.split('-')[1].split('.')[0])
+            )
+        )
+
+        # Generate the img tag with the largest image and sizes attribute
+        html += f'<img src="{largest_image}" alt="Autoservis" sizes="{self.sizes}">'
+        html += "</picture>"
+        
+
+        return html
+    
+    def generate_simple_HTML(self, names: list, width: int, height: int) -> str:
+        """
+        Generate a simple HTML picture element with a single source element for each format and an img tag
+        """
+        html = "<picture>"
+
+        # Generate a single source element for each format
+        for name in names:
+            format = name.split('.')[0]
+            html += f'<source srcset="{name}" type="image/{format}">'
+        
+        # Generate the img tag with the first (largest) image in the list
+        html += f'<img src="{names[0]}" alt="Autoservis" width={width} height={height}>'
+
+        html += "</picture>"
+        return html
+
+    def get_sizes(self, max_width: int, available_sizes: dict, min_step_size, responsive: bool) -> list:
+        """
+        Generate size for responsive images
+        """
+        sizes = []
+        if responsive:
+            sizes = [size for size in available_sizes.values() if size < (max_width - min_step_size)]
+        sizes.append(max_width)
+        return sizes
+    
+    def generate_zip(self, image_set: list, html: str, generate_HTML: bool = True, generate_CSS: bool = True):
+        """
+        Compress the images into a zip file in memory
+        """
+        # generate zip file
+        zip_byte_arr = io.BytesIO()
+        with zipfile.ZipFile(zip_byte_arr, "w") as zip:
+            for filename, img_byte_arr in image_set:
+                zip.writestr(filename, img_byte_arr.read())
+            if generate_HTML:
+                zip.writestr("index.html", html)
+        
+        zip_byte_arr.seek(0)
+        return zip_byte_arr
+
+    def generate_image_set(self, file: UploadFile = File(...), name:str = "") -> list:
+        """
+        Generate a set of images with different sizes and formats from the uploaded image. 
+        """
+        # set the name of the file (same as input filename if not provided)
+        name = name if name != "" else os.path.splitext(file.filename)[0]
+        img = Image.open(file.file)
+        sizes = self.get_sizes(self.max_width, self.sizes, self.minimmum_step_size, self.responsive)
+        
+        image_set = []
+
+        # create a list of images with different sizes and formats
+        for size in sizes:
+            for format in self.available_formats:
+                img_resized = img.resize((size, int(size * img.height / img.width)))
+                    
+                # Convert RGBA to RGB if saving as JPEG
+                if format in ["jpg", "jpeg"] and img_resized.mode == 'RGBA':
+                    img_resized = img_resized.convert('RGB')
+                
+                output_filename = f"{name}-{size}.{format}"
+
+                # save to in-memory byte array
+                img_byte_arr = io.BytesIO()
+                img_resized.save(img_byte_arr, format=format.upper() if format != "jpeg" else "JPEG")
+                img_byte_arr.seek(0)
+                image_set.append((output_filename, img_byte_arr))
+
+                # generate HTML sources
+                if self.responsive:
+                    self.html_sources[format].append(f"{output_filename} {size}w")
+                else:
+                    self.names.append(output_filename)
+                
+            # generate HTML sources
+            if self.responsive:
+                html = self.generate_HTML(self.available_formats, self.html_sources)
+            else:
+                html = self.generate_simple_HTML(self.names, size, img_resized.height)
+
+        
+        # generate zip file
+        zip_byte_arr = self.generate_zip(image_set, html, self.allow_HTML_generation, self.allow_CSS_generation)
+
+        return zip_byte_arr
--- main.py
+++ main.py
@@ -1,172 +1,23 @@
+from generator import Image_generator
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
-from PIL import Image
-import zipfile
 import os
 import io
 
 app = FastAPI()
 
-sizes = {
-        "small": 420,
-        "medium": 960,
-        "extra_large": 1920
-}
-
-html = "<picture>"
-html_sources = {
-}
-
-names = []
-
-minimmum_step_size = 300
-
-def generateHTML(available_formats: list, html_sources: dict):
-    html = "<picture>"
-
-    # Generate srcset attributes for each format in reverse order
-    for format, sources in reversed(html_sources.items()):
-        srcset = ", ".join(sources)
-        html += f'<source srcset="{srcset}" type="image/{format}">'
-
-    # Find the largest image across all formats for the img src attribute
-    largest_image = max(
-        html_sources[available_formats[0]],
-        key=lambda x: int(x.split('-')[1].split('.')[0])
-    ).split(' ')[0]
-
-    # Generate sizes attribute based on the available sizes in html_sources
-    sizes = ", ".join(
-        f"(max-width: {int(img.split('-')[1].split('.')[0])}px) {int(img.split('-')[1].split('.')[0])}px"
-        for img in sorted(
-            {img for imgs in html_sources.values() for img in imgs},
-            key=lambda x: int(x.split('-')[1].split('.')[0])
-        )
-    )
-
-    html += f'<img src="{largest_image}" alt="Autoservis" sizes="{sizes}">'
-    html += "</picture>"
-
-    return html
-
-def generateSimpleHTML(names: list, size: int, height: int):
-    html = "<picture>"
-
-    # Generate a single source element for each format
-    for name in names:
-        format = name.split('.')[0]
-        html += f'<source srcset="{name}" type="image/{format}">'
-    
-    html += f'<img src="{names[0]}" alt="Autoservis" width={size} height={height}>'
-
-
-    html += "</picture>"
-    return html
 
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the Image Set Generator API"}
 
-@app.post("/generate-image-set-responsive/")
-async def generate_image_set(max_width: int, file: UploadFile = File(...), name:str = "", transparent: bool = False):
+@app.post("/generate/")
+async def generate_image_set(max_width: int, image: UploadFile = File(...), image_name: str = "", multiple_sizes: bool = True, transparent: bool = False, generate_HTML: bool = True, generate_CSS: bool = True):
     """
-    Generate a set of images with different sizes from the uploaded image. 
-    LEAVE THE NAME EMPTY IF YOU WANT TO KEEP THE SAME NAME AS THE UPLOADED FILE.
-    """
-    try:
-        # Open the uploaded image
-        img = Image.open(file.file)
-        name = name if name != "" else os.path.splitext(file.filename)[0]
-
-        formats = ["png" if transparent else "jpeg", "webp"]
-        for format in formats:
-            html_sources[format] = []
-        _sizes = [size for size in sizes.values() if size < (max_width - minimmum_step_size)]
-        _sizes.append(max_width)
-
-        # Generate and save images for each size
-        image_set = []
-        for size in _sizes:
-            for format in formats:
-                img_resized = img.resize((size, int(size * img.height / img.width)))
-                    
-                # Convert RGBA to RGB if saving as JPEG
-                if format in ["jpg", "jpeg"] and img_resized.mode == 'RGBA':
-                    img_resized = img_resized.convert('RGB')
-                    
-                output_filename = f"{name}-{size}.{format}"
-                img_byte_arr = io.BytesIO()
-                img_resized.save(img_byte_arr, format=format.upper() if format != "jpeg" else "JPEG")
-                img_byte_arr.seek(0)
-                image_set.append((output_filename, img_byte_arr, size)) 
-
-                html_sources[format].append(f"{output_filename} {size}w")
-        
-
-        html = generateSimpleHTML(formats, html_sources)
-
-        # Compress the images into a zip file in memory
-        zip_byte_arr = io.BytesIO()
-        with zipfile.ZipFile(zip_byte_arr, "w") as zip:
-            for filename, img_byte_arr, size in image_set:
-                zip.writestr(filename, img_byte_arr.read())
-            zip.writestr("index.html", html)
-        zip_byte_arr.seek(0)
-
-        return StreamingResponse(zip_byte_arr, media_type="application/zip", headers={"Content-Disposition": "attachment; filename=image_set.zip"})
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing image: {e}")
-
-@app.post("/generate-image-set/")
-async def generate_image_set(desired_width: int, file: UploadFile = File(...), name: str = "", transparent: bool = False):
-    """
-    Generate two images in different formats at the desired size from the uploaded image.
-    LEAVE THE NAME EMPTY IF YOU WANT TO KEEP THE SAME NAME AS THE UPLOADED FILE.
+    Generate a set of images with different sizes and formats from the uploaded image.
     """
     try:
-        # Open the uploaded image
-        img = Image.open(file.file)
-        name = name if name != "" else os.path.splitext(file.filename)[0]
-
-        formats = ["png" if transparent else "jpeg", "webp"]
-        for format in formats:
-            html_sources[format] = []
-
-        # Resize the image to the desired width
-        img_resized = img.resize((desired_width, int(desired_width * img.height / img.width)))
-
-        # Generate and save images for each format
-        image_set = []
-        for format in formats:
-            # Convert RGBA to RGB if saving as JPEG
-            if format in ["jpg", "jpeg"] and img_resized.mode == 'RGBA':
-                img_resized = img_resized.convert('RGB')
-
-            output_filename = f"{name}.{format}"
-            img_byte_arr = io.BytesIO()
-            img_resized.save(img_byte_arr, format=format.upper() if format != "jpeg" else "JPEG")
-            img_byte_arr.seek(0)
-            image_set.append((output_filename, img_byte_arr))
-
-            names.append(output_filename)
-
-        html = generateSimpleHTML(names, img_resized.width, img_resized.height)
-
-        # Compress the images into a zip file in memory
-        zip_byte_arr = io.BytesIO()
-        with zipfile.ZipFile(zip_byte_arr, "w") as zip:
-            for filename, img_byte_arr in image_set:
-                zip.writestr(filename, img_byte_arr.read())
-            zip.writestr("index.html", html)
-        zip_byte_arr.seek(0)
-
-        return StreamingResponse(zip_byte_arr, media_type="application/zip", headers={"Content-Disposition": "attachment; filename=single_image.zip"})
+        zip = Image_generator(transparent, multiple_sizes, max_width, generate_HTML, generate_CSS).generate_image_set(image, image_name)
+        return StreamingResponse(io.BytesIO(zip.read()), media_type="application/zip", headers={"Content-Disposition": "attachment; filename=image_set.zip"})
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing image: {e}")
-
-@app.get("/get-sizes/")
-async def get_sizes():
-    """
-    Get the available sizes for the image set.
-    """
-    return sizes
+        raise HTTPException(status_code=400, detail=f"An error occurred: {e}")
